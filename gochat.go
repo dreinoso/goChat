15d40c5d01aef5aa62c24fa8d6c9c460d75bfa2c
Denis Reinoso <reinoso.denis@gmail.com>
2018-10-02 00:35:30 -0300

    _#2 Make input from console not cut off when receiving a message
    The problem to achieve this was to cache all the bytes written
    at the time a message arrives. A way to solve this is getting
    one char at the time and save that in other place (in this
    case using a channel).
    
    Another channel is used to know when a new message has been
    received, so can clean the line, print the message and print
    the text the user has been writing at the moment.
    
    Also:
      - Add constants
      - Close resources
    
    Signed-off-by: Denis Reinoso <reinoso.denis@gmail.com>

--- gochat.go
+++ gochat.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net"
+	"os/exec"
 	"fmt"
 	"bufio"
 	"os"
@@ -12,6 +13,8 @@ import (
 const defaultPort = "8080"
 const defaultIP = "127.0.0.1"
 const addressConnector = ":"
+const senderPrefix = "Me: "
+const receiverPrefix = "Received: "
 
 func main() {
 	isServer := false
@@ -22,27 +25,55 @@ func main() {
 		address = defaultIP + addressConnector + defaultPort
 	}
 
-	var connection net.Conn 
+	var connection net.Conn
+	var listener net.Listener
+
 	if isServer {
 		// listen on all interfaces
-		listenter, _ := net.Listen("tcp", address)
-	
-		// accept connection on port
-		connection, _ = listenter.Accept()
+		listener, _ = net.Listen("tcp", address)
+		connection, _ = listener.Accept()
 	} else { // it's a client
 		// connect to this socket
 		connection, _ = net.Dial("tcp", address)
 	}
 
-	go receiveMessages(connection)
+	textChannel := make(chan string)
+	messageReceivedFlag := make(chan bool)
 
+	defer func() {
+		close(textChannel)
+		close(messageReceivedFlag)
+		connection.Close()
+		listener.Close()
+	}()
+
+	go receiveMessages(connection, messageReceivedFlag)
+	go printCurrentText(textChannel, messageReceivedFlag)
+
+	// disable input buffering to be able to read one char at the time, it's
+	// needed to be able caching the characters writen when receiving a message
+	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+	// do not display entered characters on the screen
+	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+
+	// read from console and send messages
+	var byteRead = make([]byte, 1)
 	for { 
-		// read in input from stdin
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Text to send: ")
-		text, _ := reader.ReadString('\n')
-		// send to socket
-		fmt.Fprintf(connection, text + "\n")
+		fmt.Print(senderPrefix)
+		os.Stdin.Read(byteRead)
+		var inputMessage string
+		for ( string(byteRead) != "\n") {
+			fmt.Print(string(byteRead))
+			textChannel <- string(byteRead)
+			inputMessage += string(byteRead)
+			os.Stdin.Read(byteRead)
+		}
+
+		// send message via socket
+		fmt.Fprintf(connection, inputMessage + "\n")
+		inputMessage = ""
+		messageReceivedFlag <- false
+		fmt.Println()
 	}
 }
 
@@ -53,8 +84,8 @@ func getAddres()(string, bool) {
     case 2:
     	_, err := strconv.Atoi(os.Args[1]);
     	if err == nil {
-        	address = os.Args[1] + addressConnector + defaultPort
-    	} else if(strings.ToLower(os.Args[1]) == "-c") {
+			address = os.Args[1] + addressConnector + defaultPort
+		} else if(strings.ToLower(os.Args[1]) == "-s") {
 			address = defaultIP + addressConnector + defaultPort
 			isServer = true
       	} else {
@@ -69,7 +100,7 @@ func getAddres()(string, bool) {
          exitProgram()
      	}
     case 4:
-      	isErr1 := strings.ToLower(os.Args[1]) == "-c"
+		isErr1 := strings.ToLower(os.Args[1]) == "-s"
       	_, err2 := strconv.Atoi(os.Args[2]);
       	_, err3 := strconv.Atoi(os.Args[3]);
       	if isErr1 && err2 == nil && err3 == nil {
@@ -84,24 +115,37 @@ func getAddres()(string, bool) {
   return address, isServer
 }
 
-func receiveMessages(connection net.Conn) {
-	reader := bufio.NewReader(os.Stdin)
-	
+func receiveMessages(connection net.Conn, messageReceivedFlag chan bool) {
 	for {
 		// will listen for message to process ending in newline (\n)
 		message, _ := bufio.NewReader(connection).ReadString('\n')
 		fmt.Printf("%c[2K", 27)
-		fmt.Println("\rMessage received: " + string(message))
-		var inputMsgCached []byte
-		reader.Read(inputMsgCached)
-		fmt.Print(inputMsgCached)
+		fmt.Print("\r" + receiverPrefix + string(message))
+		messageReceivedFlag <- true
+	}
+}
+
+func printCurrentText(textChannel chan string, messageReceivedFlag chan bool) {
+	var writenUntilNow string
+	for {
+		select {
+		case charReceived := <- textChannel:
+			writenUntilNow += charReceived
+			charReceived = ""
+		case isMEssageReceived := <- messageReceivedFlag:
+			if isMEssageReceived {
+				fmt.Print(senderPrefix + writenUntilNow)
+			}
+			writenUntilNow = ""
+		default:
+		}
 	}
 }
 
 func exitProgram() {
 	// print usage
 	fmt.Println("bash: gochat: " + os.Args[1] + ": invalid option")
-	fmt.Println("gochat: usage: gochat [-c] [dir ip] [port]")
+	fmt.Println("gochat: usage: gochat [-s] [dir ip] [port]")
 
 	os.Exit(0)
 }
